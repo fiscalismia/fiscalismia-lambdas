c6fc5ad6c2accd33c63ae47f5ae4ac82eb332e5f
hangrybear666 <hangrybear666+github@proton.me>
2026-02-19 12:31:47 +0100

    Updates data extraction from xlsx

--- functions/python/Fiscalismia_RawDataETL/extract_transform.py
+++ functions/python/Fiscalismia_RawDataETL/extract_transform.py
@@ -11,13 +11,14 @@ def extract_and_transform_to_tsv(
 ):
   row_count = sheet.shape[0]
   col_count = int(sheet.shape[1])
-  output = json.dumps(
+  debug_output = json.dumps(
   {
-        "size": f"{sheet.size} bytes",
-        "rows": row_count,
-        "columns": col_count,
-        "header": sheet.iloc[3, :].to_dict(),
-        "tail": sheet.tail(1).astype(str).to_dict(orient="records"),
+    "size": f"{sheet.size} bytes",
+    "rows": row_count,
+    "columns": col_count,
+    "header": sheet.iloc[3, :].to_dict(),
+    "tail": sheet.tail(1).astype(str).to_dict(orient="records"),
   })
+  logger.debug("Running sanity check on Finance sheet", extra={"sanity_check": debug_output})
   add_time_analysis_entry(timedelta_analysis, start_time, "finalized TSV extraction from Finance sheet")
-  return output
+  return "output"
