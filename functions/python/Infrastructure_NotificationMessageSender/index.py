8a7b083b6ad54ca14e39f93450ff8145f1ed6ae4
hangrybear666 <hangrybear666+github@proton.me>
2026-06-11 20:40:54 +0200

    Sending telegram bot api msgs via requests lib

--- functions/python/Infrastructure_NotificationMessageSender/index.py
+++ functions/python/Infrastructure_NotificationMessageSender/index.py
@@ -1,47 +1,95 @@
 import json
-import os
+import requests
+from aws_lambda_powertools import Logger
+from aws_lambda_powertools.utilities import parameters
+from zoneinfo import ZoneInfo
 from datetime import datetime
 
-print('Loading function: Infrastructure_NotificationMessageSender')
-
+logger = Logger(service="Infrastructure_NotificationMessageSender")
+TELEGRAM_BOT_ENDPOINT_BASE_URL: str = "api.telegram.org/bot"
 def lambda_handler(event, context):
     """
     Lambda function to send notification messages.
     Triggered by SNS to process and forward notifications.
     """
     function_name = context.function_name
-    request_id = context.request_id
+    request_id = context.aws_request_id
+    current_time = datetime.now(tz=ZoneInfo("Europe/Berlin"))
 
-    print(f"Function: {function_name} | Request ID: {request_id}")
-    print(f"Invoked at: {datetime.utcnow().isoformat()}")
+    logger.debug("Function invoked", extra={"function_name": function_name, "request_id": request_id, "invoked_at": current_time })
 
-    # Extract SNS message
     try:
+        # query SecureString from AWS Parameter Store
+        TELEGRAM_API_TOKEN = parameters.get_parameter(
+            "/notifications/telegram/FISCALISMIA_MSG_TELEGRAM_API_TOKEN",
+            decrypt=True
+        )
+        TELEGRAM_CHAT_ID = parameters.get_parameter(
+            "/notifications/telegram/ADMIN_TELEGRAM_CHAT_ID",
+            decrypt=True
+        )
+        if TELEGRAM_API_TOKEN is None or TELEGRAM_CHAT_ID is None:
+            error_msg = "Telegram SecureStrings could not be extracted from parameter store."
+            logger.error(error_msg)
+            return {
+                "statusCode": 400,
+                "body": json.dumps({"error": error_msg})
+            }
         if 'Records' in event and len(event['Records']) > 0:
+            # Decoding received SNS message and subject
             sns_message = event['Records'][0]['Sns']['Message']
-            sns_subject = event['Records'][0]['Sns'].get('Subject', 'No Subject')
+            sns_subject = event['Records'][0]['Sns']['Subject']
+            sns_timestamp = event['Records'][0]['Sns']['Timestamp']
+            sns_topic_arn = event['Records'][0]['Sns']['TopicArn']
+            logger.info("SNS Message Received.", extra={"topic_arn": sns_topic_arn,  "sns_message": sns_message, "timestamp": sns_timestamp, "subject": sns_subject})
 
-            print(f"SNS Subject: {sns_subject}")
-            print(f"SNS Message: {sns_message}")
+            # building telegram endpoint and payload
+            url = f"https://{TELEGRAM_BOT_ENDPOINT_BASE_URL}{TELEGRAM_API_TOKEN}/sendMessage"
+            logger.debug("Sending Notification to Telegram API", extra={"telegram_endpoint": url})
 
-            # TODO: Implement your notification logic here
-            # Example: Send email, post to Slack, write to database, etc.
+            headers = {"content-type": "application/json"}
+            payload = {
+                "chat_id": TELEGRAM_CHAT_ID,
+                "parse_mode": "HTML",
+                "text": sns_message
+            }
+            # Sending request payload to telegram api
+            response = requests.post(url, json=payload, headers=headers)
+            data = json.loads(response.text)
+            telegram_status = data.get("ok", None)
+            telegram_result = data.get("result", None)
+            logger.debug("Sent Telegram request.", extra={"status": telegram_status, "result": telegram_result})
 
+            if telegram_status is True:
+                telegram_sender_name = data.get("result", None).get("from", None).get("first_name", None)
+                telegram_receiver_username = data.get("result", None).get("chat", None).get("username", None)
+                logger.info("Telegram Notification sent successfully", extra={"sender_name": telegram_sender_name,"receiver_username": telegram_receiver_username})
+            else:
+                logger.error("Telegram Notification not sent successfully",extra={"status": telegram_status, "result": telegram_result})
+                return {
+                    "statusCode": 400,
+                    "body": json.dumps({"error": "Telegram Notification not sent successfully"})
+                }
             return {
                 "statusCode": 200,
                 "body": json.dumps({
                     "message": "Notification sent successfully",
                     "function": function_name,
                     "subject": sns_subject,
-                    "notification_content": sns_message
+                    "notification_content": sns_message,
+                    "telegram_sender_name": telegram_sender_name,
+                    "telegram_receiver_username": telegram_receiver_username,
                 })
             }
         else:
-            print("No SNS records found in event")
+            logger.error("No SNS records found in event")
             return {
                 "statusCode": 400,
-                "body": json.dumps({"error": "Invalid event structure"})
+                "body": json.dumps({"error": "Invalid SNS event structure"})
             }
     except Exception as e:
-        print(f"Error processing notification: {str(e)}")
-        raise
+        logger.error("Unexpected error during NotificationMessageSender", extra={"error": str(e)})
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
